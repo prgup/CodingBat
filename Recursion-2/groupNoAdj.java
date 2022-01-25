e70d95eea813ab622d4f7a66e28251ca337e91dd
prgup <praveenguptaemail@gmail.com>
2022-01-25 10:23:19 +0530

    Added comments

--- Recursion-2/groupNoAdj.java
+++ Recursion-2/groupNoAdj.java
@@ -1,3 +1,11 @@
+/*Given an array of ints, is it possible to choose a group of some 
+of the ints, such that the group sums to the given target with this
+ additional constraint: If a value in the array is chosen to be in 
+ the group, the value immediately following it in the array must not
+  be chosen. (No loops needed.)
+
+URL: https://codingbat.com/prob/p169605*/
+
 public boolean groupNoAdj(int start, int[] nums, int target) {
   if (target==0)
   return true;
