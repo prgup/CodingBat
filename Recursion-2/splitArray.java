e65bf43112bdf18af89d2523712c25b9ecf6ca4a
prgup <praveenguptaemail@gmail.com>
2022-01-28 19:02:47 +0530

    Added splitArray 1.1

--- Recursion-2/splitArray.java
+++ Recursion-2/splitArray.java
@@ -4,6 +4,24 @@ public boolean splitArray(int[] nums) {
 public boolean sumEqual(int sum1, int sum2, int start, int [] nums){
   if (start >=nums.length)
   return sum1==sum2;
-  return (sumEqual(sum1 + nums[start], sum2, start+1, nums)) || (sumEqual(sum1 , sum2 + nums[start], start+1, nums));
+
+  if (sumEqual(sum1 + nums[start], sum2, start+1, nums))
+    return true;
+
+  if (sumEqual(sum1 , sum2 + nums[start], start+1, nums))
+    return true;
+
+  return false;
   
 }
+
+/*
+public boolean splitArray(int[] nums) {
+  return sumEqual(0,0, 0, nums);
+}
+public boolean sumEqual(int sum1, int sum2, int start, int [] nums){
+  if (start >=nums.length)
+  return sum1==sum2;
+  return (sumEqual(sum1 + nums[start], sum2, start+1, nums)) || (sumEqual(sum1 , sum2 + nums[start], start+1, nums));
+  
+}*/
