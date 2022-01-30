732f504d0f777230a19ec133d7a5d7b45c47df32
prgup <praveenguptaemail@gmail.com>
2022-01-30 19:49:36 +0530

    splitOdd10 simple

--- Recursion-2/splitOdd10.java
+++ Recursion-2/splitOdd10.java
@@ -1,3 +1,20 @@
+public boolean splitOdd10(int[] nums) {
+  return checkSplit(0,0, nums, 0);
+}
+boolean checkSplit(int sumC, int oddC, int [] nums, int length ){
+  if (length >= nums.length)
+    return (sumC%10)==0 && oddC%2==1;
+  
+  if (checkSplit(sumC+nums[length], oddC, nums, length+1))
+  return true;
+  
+  if (checkSplit(sumC, oddC+nums[length], nums, length+1))
+  return true;
+  
+  return false;
+}
+
+
 /*
 Less Verbal Approch
 
